cbecd59dd17b98ce5a0b7b22adad12a4736352fb
Fredd <fredd@neosdev.com.br>
2019-12-06 16:54:42 -0300

    validate email

--- src/app/controllers/UserController.js
+++ src/app/controllers/UserController.js
@@ -2,6 +2,10 @@ import User from '../models/User';
 
 class UserController {
   async store(req, res) {
+    const userExist = await User.findOne({ where: { email: req.body.email } });
+    if (userExist) {
+      return res.status(400).json({ error: 'User email already exist' });
+    }
     const user = await User.create(req.body);
 
     return res.json({ user });
