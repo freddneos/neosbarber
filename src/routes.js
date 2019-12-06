2a0a32af3f2447e14120f8b08e52b18e180a3883
Fredd <fredd@neosdev.com.br>
2019-12-06 16:21:12 -0300

    first-insert-database

--- src/routes.js
+++ src/routes.js
@@ -1,4 +1,5 @@
 import { Router } from 'express';
+import User from './app/models/User';
 
 const routes = new Router();
 
@@ -6,7 +7,15 @@ function testMid(req, res, next) {
   console.log('URL ->  ', req.url);
   next();
 }
+// testing insert
+routes.get('/', testMid, async (req, res) => {
+  const user = await User.create({
+    name: 'Fredd',
+    email: 'fredd@neosdev.com.br',
+    password_hash: '123321223'
+  });
 
-routes.get('/', testMid, (req, res) => res.json({ message: 'hello world' }));
+  return res.json(user);
+});
 
 export default routes;
